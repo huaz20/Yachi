e5627389f4a346ff98b5531a74c54281b7a3b172
Asleepdoggy <maomaoovo@outlook.com>
2026-04-14 08:16:25 +0800

    perf: 删除冗余代码
    mainwindow.cpp中有设置过全局字体

--- main.cpp
+++ main.cpp
@@ -6,10 +6,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    //设置全局字体
-    QFont globalFont("Microsoft YaHei", 9);
-    a.setFont(globalFont);
-
     MainWindow w;
     w.show();
     return QCoreApplication::exec();
